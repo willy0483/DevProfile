a32e672bfe8d67656a73ff2efd08e0017956bea0
willy0483 <willyjensen251@gmail.com>
2025-03-03 17:01:29 +0100

    setup & i

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>homepage</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global.styled.js";
+import { theme } from "./styles/theme.Styled.js";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
